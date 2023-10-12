e284d1dce77056c36573c3b40c2879424bd8c11a
Илья Федоренко <fedorenkoilia@gmail.com>
2023-10-12 16:34:52 +0200

    Исправление flake8

--- homework.py
+++ homework.py
@@ -150,8 +150,8 @@ def main(training: Training) -> None:
 
 if __name__ == '__main__':
     PACKAGE = [('SWM', [720, 1, 80, 25, 40]),
-                ('RUN', [15000, 1, 75]),
-                ('WLK', [9000, 1, 75, 180]),]
+               ('RUN', [15000, 1, 75]),
+               ('WLK', [9000, 1, 75, 180]),]
 
 
 for workout_type, data in PACKAGE:
